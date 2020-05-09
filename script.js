800b5d58ea017d8587b38dbf2a50587bcac95283
Jakub Bauza <bulzor95@gmail.com>
2020-05-09 17:51:15 +0200

    scroll to slide reaction time increased

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ if (window.innerWidth > limit) {
     $("li#one").on("click", liOneChange);
     $("li#two").on("click", liTwoChange);
     $("li#three").on("click", liThreeChange);
-    document.addEventListener("wheel", throttle(scrollChange, 300));
+    document.addEventListener("wheel", throttle(scrollChange, 1000));
     //button one
     function liOneChange() {
       $(".pagetwo").animate(
